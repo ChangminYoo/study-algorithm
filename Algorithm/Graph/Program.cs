0cf9a73a346bddb91da31a06cdf8b965b1ccd507
YCM <min940913@naver.com>
2022-01-04 16:09:29 +0900

    2022-01-04
    우선순위 큐 구현

--- Algorithm/Graph/Program.cs
+++ Algorithm/Graph/Program.cs
@@ -167,16 +167,176 @@ namespace Graph
 		}
 	}
 
+	class TreeNode<T>
+	{
+		public T Data { get; set; }
+		public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+	}
+
+	class PrioirtyQueue<T> where T : IComparable<T>
+	{
+		List<T> _heap = new List<T>();
+
+		// O(logN)
+		public void Push(T data)
+		{
+			_heap.Add(data);
+
+			int now = _heap.Count - 1;
+
+			while (now > 0)
+			{
+				int next = (now - 1) / 2;
+				if (_heap[now].CompareTo(_heap[next]) < 0)
+					break;
+
+				T temp = _heap[now];
+				_heap[now] = _heap[next];
+				_heap[next] = temp;
+
+				now = next;
+			}
+		}
+
+		// O(logN)
+		public T Pop()
+		{
+			T ret = _heap[0];
+
+			int lastIndex = _heap.Count - 1;
+			_heap[0] = _heap[lastIndex];
+			_heap.RemoveAt(lastIndex);
+			lastIndex--;
+
+			int now = 0;
+			while (true)
+			{
+				int left = (now * 2) + 1;
+				int right = (now * 2) + 2;
+
+				int next = now;
+				if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+				{
+					next = left;
+				}
+				
+				if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+				{
+					next = right;
+				}
+
+				if (next == now) break;
+
+				T temp = _heap[now];
+				_heap[now] = _heap[next];
+				_heap[next] = temp;
+
+				now = next;
+			}
+
+			return ret;
+		}
+
+		public int Count()
+		{
+			return _heap.Count;
+		}
+	}
+
+	class Knight : IComparable<Knight>
+	{
+		public int Id { get; set; }
+
+		public int CompareTo(Knight other)
+		{
+			if (Id == other.Id)
+				return 0;
+			return Id > other.Id ? 1 : -1;
+		}
+	}
+
 	class Program
 	{
+		static TreeNode<string> MakeTree()
+		{
+			TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+			{
+				{
+					TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+					node.Children.Add(new TreeNode<string>() { Data = "전투" });
+					node.Children.Add(new TreeNode<string>() { Data = "경제" });
+					node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+					root.Children.Add(node);
+				}
+				{
+					TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+					node.Children.Add(new TreeNode<string>() { Data = "서버" });
+					node.Children.Add(new TreeNode<string>() { Data = "클라" });
+					node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+					root.Children.Add(node);
+				}
+				{
+					TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+					node.Children.Add(new TreeNode<string>() { Data = "배경" });
+					node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+					root.Children.Add(node);
+				}
+			}
+
+			return root;
+		}
+
+		static void PrintTree(TreeNode<string> root)
+		{
+			//접근
+			Console.WriteLine(root.Data);
+			
+			foreach (TreeNode<string> child in root.Children)
+			{
+				PrintTree(child);
+			}
+		}
+
+		static int GetHeight(TreeNode<string> root)
+		{
+			int height = 0;
+			foreach (TreeNode<string> child in root.Children)
+			{
+				int newHight = GetHeight(child) + 1;
+				height = Math.Max(height, newHight);
+			}
+
+			return height;
+		}
+
 		static void Main(string[] args)
 		{
+			#region graph
 			Graph graph = new Graph();
 			// DFS
-			graph.DFS_Stack(0);
+			//graph.DFS_Stack(0);
 
 			// BFS
 			//graph.BFS(0);
+			#endregion
+
+			#region Tree
+			//TreeNode<string> root = MakeTree();
+			//PrintTree(root);
+
+			#endregion
+
+			PrioirtyQueue<Knight> q = new PrioirtyQueue<Knight>();
+			q.Push(new Knight() { Id = 20 });
+			q.Push(new Knight() { Id = 30 });
+			q.Push(new Knight() { Id = 40 });
+			q.Push(new Knight() { Id = 10 });
+			q.Push(new Knight() { Id = 05 });
+
+			while (q.Count() > 0)
+			{
+				Console.WriteLine(q.Pop().Id);
+			}
 		}
 	}
 }
