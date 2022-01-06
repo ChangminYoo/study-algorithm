f0364e6f71c741d59421d29b61a5f8f4eee4214e
YCM <min940913@naver.com>
2022-01-06 14:11:16 +0900

    2022-01-06

--- C++Practice/C++Practice/C++Practice.cpp
+++ C++Practice/C++Practice/C++Practice.cpp
@@ -1,11 +1,30 @@
-﻿#include <iostream>
-#include <cmath>
+﻿#include <cmath>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "LinkedList.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LinkedList<int> list;
+    list.Insert(0);
+    list.Insert(1);
+    list.Insert(2);
+    list.Insert(3);
+    list.Insert(5);
+
+    list.Print();
+
+    cout << endl << endl;
+    list.Delete(0);
+    list.Delete(2);
+    list.Delete(5);
+    list.Delete(3);
+    list.Delete(1);
+
+    list.Print();
+
+    list.Delete(2);
+    list.Print();
 }
 
--- C++Practice/C++Practice/LinkedList.h
+++ C++Practice/C++Practice/LinkedList.h
@@ -1,17 +1,127 @@
-#ifndef __DOUBLE_LINGED_LIST_H__
-#define __DOUBLE_LINGED_LIST_H__
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
 
+#include <iostream>
+using namespace std;
+
+template<typename T>
 class Node
 {
-	int data;
+public:
+	T data;
 	Node* next;
 	Node* prev;
+
+	Node<T>(T data) : data(data){}
 };
 
-
+template<typename T>
 class LinkedList
 {
+private:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int _size;
 
+public:
+	LinkedList()
+	{
+		_head = nullptr;
+		_tail = nullptr;
+		_size = 0;
+	}
+
+	void Insert(T t);
+	void Delete(T t);
+	int GetSize() const { return _size; }
+
+	void Print()
+	{
+		Node<T>* current = _head;
+		while (current != nullptr)
+		{
+			cout << current->data << endl;		
+			current = current->next;
+		}
+	}
 };
 
+
 #endif
+
+template<typename T>
+inline void LinkedList<T>::Insert(T t)
+{
+	Node<T>* newData = new Node<T>(t);
+	_size++;
+	if (_head == nullptr)
+	{
+		_head = newData;
+		_head->next = nullptr;
+		_head->prev = nullptr;
+	}
+
+
+	if (_tail == nullptr)
+	{
+		_tail = newData;
+		_tail->next = nullptr;
+		_tail->prev = nullptr;
+	}
+	else
+	{
+		_tail->next = newData;
+		newData->prev = _tail;
+		_tail = newData;
+	}
+}
+
+template<typename T>
+inline void LinkedList<T>::Delete(T t)
+{
+	if (_head == nullptr) { cout << "Linked List is Empty" << endl; return; }
+	_size--;
+	Node<T>* deleteData = _head;
+	
+	if (t == deleteData->data)
+	{
+		if (_head->next == nullptr)
+		{
+			delete _head;
+			_head = nullptr;
+			return;
+		}
+
+		_head->next->prev = nullptr;
+		_head = _head->next;
+		delete deleteData;
+		return;
+	}
+
+	while (true)
+	{
+		deleteData = deleteData->next;
+		if (deleteData->data == t)
+		{
+			if (deleteData == _tail)
+			{
+				_tail = deleteData->prev;
+				_tail->next = nullptr;
+			}
+			else
+			{
+				deleteData->prev->next = deleteData->next;
+				deleteData->next->prev = deleteData->prev;
+			}
+			delete deleteData;
+			break;
+		}
+
+		if (deleteData == nullptr)
+		{
+			cout << "There is No Data" << endl;
+			break;
+		}
+	}
+
+}
