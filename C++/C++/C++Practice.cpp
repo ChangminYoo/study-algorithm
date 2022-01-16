c2f643d822afa7862cb5962f1900ef60bb3ed3af
YCM <min940913@naver.com>
2022-01-17 08:48:20 +0900

    2022-01-17
    gitIgnore

---
+++ C++/C++/C++Practice.cpp
@@ -0,0 +1,48 @@
+﻿#include <cmath>
+#include <string>
+#include <algorithm>
+#include "LinkedList.h"
+#include "Dijkstra.h"
+
+void LinkedListTest()
+{
+    LinkedList<int> list;
+    list.Insert(0);
+    list.Insert(1);
+    list.Insert(2);
+    list.Insert(3);
+    list.Insert(5);
+
+    list.Print();
+
+    cout << endl << endl;
+    list.Delete(0);
+    list.Delete(2);
+    list.Delete(5);
+    list.Delete(3);
+    list.Delete(1);
+
+    list.Print();
+
+    list.Delete(2);
+    list.Print();
+}
+
+void GraphTest()
+{
+    Graph* graph = new Graph();
+    //graph->InitList();
+
+    //graph->DFS(3);
+    //graph->DFSWithStack(0);
+    //graph->BFS(0);
+
+    Dijkstra* dijkstra = new Dijkstra();
+    dijkstra->Generate(0);
+}
+
+int main()
+{
+    GraphTest();
+}
+
